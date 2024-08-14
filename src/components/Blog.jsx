98d839d11c04ef37e1d8127371ffb953a484bcf9
Wachann <wachannarkhede4@gmail.com>
2024-08-14 14:28:57 +0530

    uu

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -552,7 +552,7 @@ const posts = [
           'reward earn- 3$ ',
         date: 'june 7, 2024',
         datetime: '2020-03-16',
-        category: { title: 'Pay with PayPal', href: 'https://www.paypal.com/paypalme/blejndevid' },
+        category: { title: 'Pay with PayPal', href: 'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=blejndevid@gmail.com&amount=3.00&currency_code=USD' },
   
         author: {
           name: 'tw1nkLerin',
